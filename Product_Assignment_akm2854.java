7704a352aa03baf6fb918831b01decc8157076b0
anamonge <anamonge@utexas.edu>
2016-11-22 14:58:43 -0600

    completed assignment locally

---
+++ Product_Assignment_akm2854.java
@@ -0,0 +1,7 @@
+// Create a Java class representing a Product
+// It should be based on the template here.
+// It should be named Product_Assignment_akm2854.java
+
+class Product_Assignment_akm2854 {
+	private String eid = "akm2854";
+}
